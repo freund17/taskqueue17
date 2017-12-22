2f84ab0432f482edd5fedc61c1bd774587c6108d
freund17 <lukas.aymans@googlemail.com>
2017-12-22 12:30:59 +0100

    activated mocha-linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
-    "extends": "standard"
+    extends: 'standard',
+    plugins: [
+        'mocha'
+    ],
+    rules: {
+        'mocha/no-identical-title': ['error'],
+        'mocha/no-mocha-arrows': ['error'],
+        'mocha/no-nested-tests': ['error'],
+        'mocha/no-pending-tests': ['warn'],
+        'mocha/no-synchronous-tests': ['error', { allowed: ['async'] }]
+    }
 };
